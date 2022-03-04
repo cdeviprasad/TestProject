c3111f66c3ba8cbacf23c13ea5d13a246c0016ea
deviprasad <deviprasaddsp432@gmail.com>
2022-03-05 00:16:49 +0530

    first commit by dsp

---
+++ Test.project/src/main/java/test/project/Test.java
@@ -0,0 +1,18 @@
+package test.project;
+
+public class Test {
+	
+	public static void method() {
+		
+		System.out.println("Thanks for calling me ........!");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Test.method();
+		
+		
+	}
+
+}
